d31394bdc0f7b4d74a7c738454e2b59d3e524e7f
pramod <pramod@open-insights.com>
2020-08-11 21:27:21 +0530

    29 Assignment Review

---
+++ src/main/java/com/springframework/msscbrewery/services/BeerServiceImpl.java
@@ -0,0 +1,16 @@
+package com.springframework.msscbrewery.services;
+
+import com.springframework.msscbrewery.web.model.BeerDto;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+@Service
+public class BeerServiceImpl implements BeerService {
+    @Override
+    public BeerDto getBeerById(UUID beerId) {
+        return BeerDto.builder().id(UUID.randomUUID())
+                .beerName("Galaxy cat")
+                .beerStyle("Pale Ale")
+                .build();
+    }
+}
---
+++ src/main/java/com/springframework/msscbrewery/services/CustomerServiceImpl.java
@@ -0,0 +1,16 @@
+package com.springframework.msscbrewery.services;
+
+import com.springframework.msscbrewery.web.model.CustomerDto;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+@Service
+public class CustomerServiceImpl implements CustomerService {
+    @Override
+    public CustomerDto getCustomerId(UUID customerId) {
+        return CustomerDto.builder()
+                .id(UUID.randomUUID())
+                .name("Joe Buck")
+                .build();
+    }
+}
